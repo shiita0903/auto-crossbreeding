389df459f4e8d1344ef4aba3d218d3ea1bf35fc6
huchenlei <hno3.hcl.hcl.hcl@gmail.com>
2022-01-18 19:44:03 -0500

    revise autoSpread

--- autoSpread.lua
+++ autoSpread.lua
@@ -1,3 +1,5 @@
+-- Set farmSize=3, and storageFarmSize=9 to use this script.
+
 local database = require("database")
 local gps = require("gps")
 local posUtil = require("posUtil")
@@ -7,53 +9,121 @@ local config = require("config")
 
 local args = {...}
 
-local function spreadOnce()
-    for slot=2, config.farmSize^2, 2 do
-        gps.go(posUtil.farmToGlobal(slot))
-        local crop = scanner.scan()
-        if crop.name == "air" then
-            action.placeCropStick(2)
-        elseif (not config.assumeNoBareStick) and crop.name == "crop" then
-            action.placeCropStick()
-        elseif crop.isCrop then
-            if crop.name == "weed" or crop.gr > 23 or
-              (crop.name == "venomilia" and crop.gr > 7) then
-                action.deweed()
-                action.placeCropStick()
-            elseif crop.name == database.getFarm()[1].name and
-                  (not config.bestStatWhileSpreading or (crop.gr >= 21 and crop.ga == 31)) then
-                local nextMultifarmPos = database.nextMultifarmPos()
-                if nextMultifarmPos then
-                    action.transplantToMultifarm(posUtil.farmToGlobal(slot), nextMultifarmPos)
-                    action.placeCropStick(2)
-                    database.updateMultifarm(nextMultifarmPos)
-                else
-                    return true
-                end
-            else
-                action.deweed()
-                action.placeCropStick()
-            end
+--[[ 
+    The script aims to transfer stats from a parent crop type to the crop type 
+    desired but have low stats.
+    
+    [P, _, X],
+    [_, T, _],
+    [X, _, P],
+    where P represents parent crop, T represents target crop. X represents a non-farmland
+    block. 
+    
+    According to IC2 breeding rule, The offspring of 2 plants have 45% chance to be either
+    the parent plants, and 10% chance to be a new type of crop.
+ ]]
+
+ local parentCrop;
+ local targetCrop;
+ -- The min stats requirement for target crop to be put into storage farm.
+ local targetCropMinStats = 46;
+ -- Current stats of target crop in the breeding cell.
+ local targetCropCurrentStats = 0;
+ 
+ local TARGETCROP_SLOT = 5;
+
+local function isWeed(crop)
+    return crop.name == "weed" or 
+        crop.name == "Grass" or
+        crop.gr > 21 or 
+        (crop.name == "venomilia" and crop.gr > 7);
+end
+
+ local function checkChildren(slot, crop)
+    if crop.name == "air" then
+        action.placeCropStick(2);
+        return;
+    end
+
+    if (not config.assumeNoBareStick) and crop.name == "crop" then
+        action.placeCropStick();
+        return;
+    end
+
+    if ~crop.isCrop then
+        return;
+    end
+
+    if isWeed(crop) then
+        action.deweed();
+        action.placeCropStick();
+        return;
+    end
+
+    if crop.name == targetCrop then
+        -- Populate breeding cells with high stats crop as priority.
+        if calculateStats(crop) >= targetCropCurrentStats then
+            action.transplant(posUtil.farmToGlobal(slot), posUtil.farmToGlobal(TARGETCROP_SLOT));
+            targetCropCurrentStats = calculateStats(crop);
+            return;
         end
+        
+        if calculateStats(crop) >= targetCropMinStats then
+            action.transplant(posUtil.farmToGlobal(slot), database.nextStorageSlot());
+            action.placeCropStick(2);
+            return;
+        end
+    end
+    
+    action.deweed();
+    action.placeCropStick();
+ end
+
+ function calculateStats(crop)
+    return crop.gr + crop.ga - crop.re;
+ end
+
+ local function posEquals(p1, p2)
+    return (p1[1] == p2[1]) and (p1[2] == p2[2]);
+ end
+
+ local function spreadOnce()
+    for slot=1, config.farmArea, 1 do
+        local farmPos = posUtil.farmToGlobal(slot);
+        local breedingCellPos = {
+            farmPos[1] % 3,
+            farmPos[2] % 3,
+        };
+
+        local isChildren = 
+            posEquals(breedingCellPos, {1, 2}) or 
+            posEquals(breedingCellPos, {2, 1}) or 
+            posEquals(breedingCellPos, {0, 2}) or 
+            posEquals(breedingCellPos, {2, 0});
+
+        gps.go(farmPos);
+        local crop = scanner.scan();
+
+        if isChildren then
+            checkChildren(crop);
+        end
+                
         if action.needCharge() then
             action.charge()
+        end
+
+        if config.takeCareOfDrops then
+            action.dumpInventory()
+        end
+
+        if #database.getStorage() >= 81 then
+            return true;
         end
     end
     return false
 end
 
 local function init()
-    database.scanFarm()
-    local multifarmPos = {}
-    if #args == 2 then
-        multifarmPos[1] = tonumber(args[1])
-        multifarmPos[2] = tonumber(args[2])
-    end
-    if multifarmPos[1] and multifarmPos[2] then
-        database.setLastMultifarmPos(multifarmPos)
-    else
-        database.scanMultifarm()
-    end
     action.restockAll()
 end
 
@@ -72,7 +142,7 @@ local function main()
         action.dumpInventory()
     end
     gps.turnTo(1)
-    print("Done.\nThe Multifarm is filled up.")
+    print("Done.\nThe Farm is filled up.")
 end
 
 main()
